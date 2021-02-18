5e5be5e0c57489ef62dc16117b9b154b6edd3be8
n0tybynature <74768895+n0tybynature@users.noreply.github.com>
2021-02-18 15:24:14 -0800

    product context added

--- src/App.js
+++ src/App.js
@@ -7,11 +7,20 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import { ProductContext } from './contexts/ProductContext';
+
+
+
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+
+		item.preventDefault();
+		setCart({cart:products});
+
 		// add the given item to the cart
 	};
 
